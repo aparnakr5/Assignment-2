ae60361df1df653eb2b3140f3c5305b16c21690e
aparnakr5 <57255095+aparnakr5@users.noreply.github.com>
2019-11-08 18:26:33 +0530

    Add files via upload

---
+++ Assignment-2/Deposit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    class Deposit
+    {
+        double principle = 1000.00d;
+        double irate = 0.05d;
+        double interest;
+
+
+        public void Calculate(double x)
+        {
+            Console.WriteLine("**********************************************************************************************");
+            Console.WriteLine(String.Format("{0,-5} |{1,-48} |{2,-25}", "Year", "Interest for the year", "End of the year amount"));
+            Console.WriteLine("**********************************************************************************************");
+            for (int i = 1; principle <= x; i++)
+
+            {
+
+                interest = principle * irate;
+
+
+                //Console.WriteLine(i + "     " + principle + "*" + irate + "=" + interest + "                      " + (principle + interest));
+                Console.WriteLine(String.Format("{0,-4}  |{1,-18} *  {2,-2}={3,-20}  |{4,-10}", i, principle, irate, interest, (principle + interest)));
+                principle += interest;
+
+
+            }
+        }
+    }
+}
---
+++ Assignment-2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your targeted matured amount?");
+            double mat_amt = double.Parse(Console.ReadLine());
+            Deposit d = new Deposit();
+            d.Calculate(mat_amt);
+            Console.ReadKey();
+        }
+    }
+}
